16f2c9d4a44cd5cfbe0d0a7eb28dc5eaee08c1b5
AlixBarreaux <you@example.com>
2025-05-19 22:55:49 +0200

    Feat: Add player movement + looking around

---
+++ scripts/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+class_name Player
+
+
+@export var camera_sensitivity: float = 0.07
+var camera_rotation_limit_x_min: float = -80.0
+var camera_rotation_limit_x_max: float = 80.0
+
+@export var speed: float = 5.5
+
+@onready var camera_3d: Camera3D = %Camera3D
+
+
+func _physics_process(_delta: float) -> void:
+	var input_direction_2d: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var input_direction_3d: Vector3 = Vector3(input_direction_2d.x, 0.0, input_direction_2d.y)
+	
+	var direction: Vector3 = self.get_transform().basis * input_direction_3d
+	
+	self.velocity.x = direction.x * self.speed
+	self.velocity.z = direction.z * self.speed
+	self.move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		self.rotation_degrees.y -= event.get_screen_relative().x * self.camera_sensitivity
+		self.camera_3d.rotation_degrees.x -= event.get_screen_relative().y * self.camera_sensitivity
+		self.camera_3d.rotation_degrees.x = clamp(self.camera_3d.rotation_degrees.x, self.camera_rotation_limit_x_min, self.camera_rotation_limit_x_max)
