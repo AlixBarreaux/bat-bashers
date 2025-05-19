f1f0bac6d4269014198e08d10b56ba7ce1c1640e
AlixBarreaux <you@example.com>
2025-05-19 15:11:14 +0200

    Feat: Add PauseMenu

---
+++ scripts/ui/pause_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+class_name PauseMenuUI
+
+
+func _ready() -> void:
+	self.hide()
+
+
+func show_menu() -> void:
+	self.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().set_pause(true)
+
+
+func hide_menu() -> void:
+	self.hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().set_pause(false)
+
+
+func toggle_menu_visible() -> void:
+	if self.is_visible():
+		self.hide_menu()
+	else:
+		self.show_menu()
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		self.toggle_menu_visible()
+
+
+func _on_resume_button_pressed() -> void:
+	self.hide_menu()
---
+++ scripts/ui/quit_to_desktop_button.gd
@@ -0,0 +1,6 @@
+extends BaseButton
+class_name QuitToDesktopButton
+
+
+func _on_pressed() -> void:
+	get_tree().quit()
